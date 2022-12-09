717b0843b56bff5be0e3c05994d5b1235e0a6cfe
Nathan FELSEMBERG <nfelsemb@e1r5p9.clusters.42paris.fr>
2022-12-09 16:28:29 +0100

    commit

---
+++ cpp01/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int	main(int argc, char **argv) {
+	int i = 1;
+	int j;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (argv[i])
+		{
+			j = -1;
+			while (argv[i][++j])
+				argv[i][j] = toupper(argv[i][j]);
+			std::cout << argv[i];
+			i++;
+		}
+		std::cout << std::endl;
+	}
+}
